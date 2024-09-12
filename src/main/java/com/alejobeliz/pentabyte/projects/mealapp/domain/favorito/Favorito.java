0a9e203dc2877627ccce78729ecc1743b5ddbcc4
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-09-12 19:29:34 -0300

    Domain favorito: crear clase Favorito

---
+++ src/main/java/com/alejobeliz/pentabyte/projects/mealapp/domain/favorito/Favorito.java
@@ -0,0 +1,27 @@
+package com.alejobeliz.pentabyte.projects.mealapp.domain.favorito;
+
+import com.alejobeliz.pentabyte.projects.mealapp.domain.cliente.Cliente;
+import com.alejobeliz.pentabyte.projects.mealapp.domain.plato.Plato;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "Favorito")
+@Table(name = "favoritos")
+public class Favorito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "plato_id")
+    private Plato plato;
+}
