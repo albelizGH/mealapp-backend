8bf42b6c7168b8ec37bc818a2df7729f1dba0743
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-09-12 19:27:29 -0300

    Domain cliente: crear clase cliente

---
+++ src/main/java/com/alejobeliz/pentabyte/projects/mealapp/domain/cliente/Cliente.java
@@ -0,0 +1,47 @@
+package com.alejobeliz.pentabyte.projects.mealapp.domain.cliente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Cliente")
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "documento")
+    private String documento;
+
+    @Column(name = "correo")
+    private String correo;
+
+    @Column(name = "contrasenia")
+    private String contrasenia;
+
+    @Column(name = "lunes")
+    private Boolean lunes;
+
+    @Column(name = "martes")
+    private Boolean martes;
+
+    @Column(name = "miercoles")
+    private Boolean miercoles;
+
+    @Column(name = "jueves")
+    private Boolean jueves;
+
+    @Column(name = "viernes")
+    private Boolean viernes;
+}
