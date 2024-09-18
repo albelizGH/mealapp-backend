612007b4a78167cf7309877bed55bbe4a310258f
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-09-18 19:45:35 -0300

    Crear clase PedidoDia

---
+++ src/main/java/com/alejobeliz/pentabyte/projects/mealapp/model/pedidosdia/PedidoDia.java
@@ -0,0 +1,27 @@
+package com.alejobeliz.pentabyte.projects.mealapp.model.pedidosdia;
+
+import com.alejobeliz.pentabyte.projects.mealapp.model.pedido.Pedido;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "PedidoDia")
+@Table(name = "pedidos_dia")
+public class PedidoDia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "dia")
+    private Dia dia;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+}
