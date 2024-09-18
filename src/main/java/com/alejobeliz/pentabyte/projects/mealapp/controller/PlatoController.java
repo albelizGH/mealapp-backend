b0621fc4c86005a123f414d785959557aa67ca5d
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-09-18 19:42:40 -0300

    Cambios en el controlador de platos

--- src/main/java/com/alejobeliz/pentabyte/projects/mealapp/controller/PlatoController.java
+++ src/main/java/com/alejobeliz/pentabyte/projects/mealapp/controller/PlatoController.java
@@ -4,13 +4,15 @@ import com.alejobeliz.pentabyte.projects.mealapp.model.plato.Plato;
 import com.alejobeliz.pentabyte.projects.mealapp.repository.PlatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/platos")
+@RequestMapping("/api/platos")
 public class PlatoController {
 
     @Autowired
@@ -20,5 +22,16 @@ public class PlatoController {
     public List<Plato> getPlatos(){
         return platoRepository.findAll();
     }
+
+
+    @GetMapping("/{id}")
+    public Plato obtenerPlatoporId(@PathVariable Long id) {
+        Optional<Plato> platoOptional = platoRepository.findById(id);
+        if (!platoOptional.isPresent()) {
+            throw new RuntimeException("No hay plato que correspondan a ese Id");
+        }
+        return platoOptional.get();
+    }
+
 }
 
