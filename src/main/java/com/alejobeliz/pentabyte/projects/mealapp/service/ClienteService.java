30dbaff09e2acadd081b4a44337e8ac24dcd1a76
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-09-26 20:43:50 -0300

    Agregar nuevos metodos en ClienteService

--- src/main/java/com/alejobeliz/pentabyte/projects/mealapp/service/ClienteService.java
+++ src/main/java/com/alejobeliz/pentabyte/projects/mealapp/service/ClienteService.java
@@ -1,14 +1,14 @@
 package com.alejobeliz.pentabyte.projects.mealapp.service;
 
-import com.alejobeliz.pentabyte.projects.mealapp.dto.Cliente.ClienteDatosPersonalesDto;
-import com.alejobeliz.pentabyte.projects.mealapp.dto.Cliente.ClienteDiasLaboralesDto;
-import com.alejobeliz.pentabyte.projects.mealapp.dto.Cliente.ClienteDto;
-import com.alejobeliz.pentabyte.projects.mealapp.mapper.Mapper;
+import com.alejobeliz.pentabyte.projects.mealapp.model.cliente.dto.ClienteDatosPersonalesDto;
+import com.alejobeliz.pentabyte.projects.mealapp.model.cliente.dto.ClienteDiasLaboralesDto;
+import com.alejobeliz.pentabyte.projects.mealapp.model.cliente.dto.ClienteDto;
 import com.alejobeliz.pentabyte.projects.mealapp.model.cliente.Cliente;
 import com.alejobeliz.pentabyte.projects.mealapp.repository.ClienteRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,25 +16,64 @@ import java.util.stream.Collectors;
 @Service
 public class ClienteService {
 
-    private ClienteRepository clienteRepository;
-    private Mapper mapper;
+    private final ClienteRepository clienteRepository;
 
     @Autowired
-    public ClienteService(ClienteRepository clienteRepository, Mapper mapper) {
+    public ClienteService(ClienteRepository clienteRepository) {
         this.clienteRepository = clienteRepository;
-        this.mapper=mapper;
     }
-
-/*    public Page<ClienteDto> getAllClientes(Pageable pageable){
-        Page<Cliente> clientes = clienteRepository.findAll(pageable);
-        return clientes.stream().map(cliente -> mapper.map(cliente,ClienteDto.class)).collect(Collectors.toList());
-    }*/
 
     //FIJARME POR QUE NO ANDA EL PAGEAB
-    public List<ClienteDto> getAllClientes(){
+    public List<ClienteDto> getAllClientes() {
         List<Cliente> clientes = clienteRepository.findAll();
-        return clientes.stream().map(cliente -> mapper.map(cliente,ClienteDto.class)).collect(Collectors.toList());
+        return clientes.stream()
+                .map(cliente -> new ClienteDto(
+                        cliente.getId().toString(),
+                        cliente.getNombre(),
+                        cliente.getApellido(),
+                        cliente.getDocumento(),
+                        new ClienteDiasLaboralesDto(cliente)
+                ))
+                .collect(Collectors.toList());
     }
+
+    public ClienteDto getCliente(Long id) {
+        ClienteDto clienteDto=clienteRepository.findById(id)
+                .map(ClienteDto::new)
+                .orElseThrow(()->new EntityNotFoundException("No se encuentra el cliente con id: " + id));
+        return clienteDto;
+    }
+
+    public ClienteDiasLaboralesDto getDiasLaborales(Long id){
+        return clienteRepository.findDiasLaboralesById(id).orElseThrow(()->new EntityNotFoundException("No se encuentra el cliente con id: " + id));
+    }
+
+    public ClienteDatosPersonalesDto getDatosPersoanles(Long id) {
+        return clienteRepository.findDatosPersonalesById(id).orElseThrow(()->new EntityNotFoundException("No se encuentra el cliente con id: " + id));
+    }
+
+    @Transactional
+    public ClienteDiasLaboralesDto setDiasLaborales(Long id, ClienteDiasLaboralesDto diasLaborales) {
+        Cliente clienteDb = clienteRepository.getClienteById(id).orElseThrow(()->new EntityNotFoundException("No se encuentra el cliente con id: " + id));
+        modificarDiasLaborales(clienteDb,diasLaborales);
+        clienteRepository.save(clienteDb);
+        return  new ClienteDiasLaboralesDto(clienteDb);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private void modificarDiasLaborales(Cliente cliente, ClienteDiasLaboralesDto diasLaboralesDto) {
         cliente.setLunes(diasLaboralesDto.lunes());
@@ -56,14 +95,10 @@ public class ClienteService {
         }
     }
 
-    public ClienteDto getCliente(Long id) {
-        ClienteDto clienteDto=clienteRepository.findById(id)
-                .map(cliente -> mapper.map(cliente,ClienteDto.class))
-                .orElseThrow(()->new EntityNotFoundException("No se encuentra el cliente con id: " + id));
-        return clienteDto;
-    }
-
-    public ClienteDiasLaboralesDto getDiasLaborales(Long id){
-        return clienteRepository.findDiasLaboralesById(id).orElseThrow(()->new EntityNotFoundException("No se encuentra el cliente con id: " + id));
+    public ClienteDatosPersonalesDto setDatosPersonales(Long id, ClienteDatosPersonalesDto datosPersonales) {
+        Cliente clienteDb = clienteRepository.getClienteById(id).orElseThrow(()->new EntityNotFoundException("No se encuentra el cliente con id: " + id));
+        modificarDatosPersonales(clienteDb,datosPersonales);
+        clienteRepository.save(clienteDb);
+        return new ClienteDatosPersonalesDto(clienteDb);
     }
 }
