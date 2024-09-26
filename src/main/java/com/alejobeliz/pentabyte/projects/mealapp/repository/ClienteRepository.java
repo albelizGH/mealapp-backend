dc369f5c2df934144df08f323bc11fb0c5c8066d
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-09-26 20:41:34 -0300

    Crear querys en ClienteRepository

--- src/main/java/com/alejobeliz/pentabyte/projects/mealapp/repository/ClienteRepository.java
+++ src/main/java/com/alejobeliz/pentabyte/projects/mealapp/repository/ClienteRepository.java
@@ -1,7 +1,9 @@
 package com.alejobeliz.pentabyte.projects.mealapp.repository;
 
-import com.alejobeliz.pentabyte.projects.mealapp.dto.Cliente.ClienteDiasLaboralesDto;
+import com.alejobeliz.pentabyte.projects.mealapp.model.cliente.dto.ClienteDatosPersonalesDto;
+import com.alejobeliz.pentabyte.projects.mealapp.model.cliente.dto.ClienteDiasLaboralesDto;
 import com.alejobeliz.pentabyte.projects.mealapp.model.cliente.Cliente;
+import com.alejobeliz.pentabyte.projects.mealapp.model.cliente.dto.ClienteDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,9 +12,25 @@ import java.util.Optional;
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
 
     @Query("""
-            Select new com.alejobeliz.pentabyte.projects.mealapp.dto.Cliente.ClienteDiasLaboralesDto(c.lunes, c.martes, c.miercoles, c.jueves, c.viernes) 
-            From Cliente c 
-            where c.id = :id
-            """)
+        SELECT new com.alejobeliz.pentabyte.projects.mealapp.model.cliente.dto.ClienteDiasLaboralesDto(c.lunes, c.martes, c.miercoles, c.jueves, c.viernes)
+        FROM Cliente c
+        WHERE c.id = :id
+        """)
     Optional<ClienteDiasLaboralesDto> findDiasLaboralesById(Long id);
+
+    @Query("""
+        SELECT new com.alejobeliz.pentabyte.projects.mealapp.model.cliente.dto.ClienteDatosPersonalesDto(c.nombre, c.apellido, c.documento)
+        FROM Cliente c
+        WHERE c.id = :id
+        """)
+    Optional<ClienteDatosPersonalesDto> findDatosPersonalesById(Long id);
+
+
+    @Query("""
+        SELECT c
+        FROM Cliente c
+        WHERE c.id = :id
+        """)
+    Optional<Cliente> getClienteById(Long id);
+
 }
