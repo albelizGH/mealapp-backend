b217be6430f900c7b61fe79435a858304cc41670
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-09-26 20:40:34 -0300

    Crear query existePedidoConEsaFeha(id,fecha) en PedidoRepository

--- src/main/java/com/alejobeliz/pentabyte/projects/mealapp/repository/PedidoRepository.java
+++ src/main/java/com/alejobeliz/pentabyte/projects/mealapp/repository/PedidoRepository.java
@@ -2,6 +2,16 @@ package com.alejobeliz.pentabyte.projects.mealapp.repository;
 
 import com.alejobeliz.pentabyte.projects.mealapp.model.pedido.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface PedidoRepository extends JpaRepository<Pedido,Long> {
+
+    @Query("""
+            SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END
+                FROM Pedido p
+                WHERE p.cliente.id = :idCliente AND p.semanaDeEntrega = :fecha
+            """)
+    boolean existePedidoConEsaFecha(Long idCliente, LocalDate fecha);
 }
