d517ed7dee129e9d200aefd6879bb3be85fb6cc7
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-09-26 20:44:18 -0300

    Agregar nuevos endpoints en PedidoController

--- src/main/java/com/alejobeliz/pentabyte/projects/mealapp/controller/PedidoController.java
+++ src/main/java/com/alejobeliz/pentabyte/projects/mealapp/controller/PedidoController.java
@@ -1,11 +1,15 @@
 package com.alejobeliz.pentabyte.projects.mealapp.controller;
 
+import com.alejobeliz.pentabyte.projects.mealapp.dto.in.PedidoSemanalDtoIn;
 import com.alejobeliz.pentabyte.projects.mealapp.model.pedido.Pedido;
 import com.alejobeliz.pentabyte.projects.mealapp.repository.PedidoRepository;
+import com.alejobeliz.pentabyte.projects.mealapp.service.PedidoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -13,11 +17,19 @@ import java.util.List;
 @RequestMapping("/api/pedidos")
 public class PedidoController {
 
-    @Autowired
-    PedidoRepository pedidoRepository;
+    private PedidoService pedidoService;
 
-    @GetMapping
-    public List<Pedido> getPedidos(){
-        return pedidoRepository.findAll();
+    @Autowired
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
     }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity crearPedidoSemanal(@RequestBody @Valid PedidoSemanalDtoIn pedido){
+        pedidoService.crearNuevoPedido(pedido);
+        return ResponseEntity.ok(pedido);
+    }
+
+
 }
