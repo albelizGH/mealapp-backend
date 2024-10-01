e5cc0b1e508bbdba627c3d79a5e9f9b5d382b322
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-10-01 15:53:38 -0300

    Agregar enpoint para hacer un get y un delete del ultimo pedido hecho

--- src/main/java/com/alejobeliz/pentabyte/projects/mealapp/controller/PedidoController.java
+++ src/main/java/com/alejobeliz/pentabyte/projects/mealapp/controller/PedidoController.java
@@ -1,17 +1,13 @@
 package com.alejobeliz.pentabyte.projects.mealapp.controller;
 
 import com.alejobeliz.pentabyte.projects.mealapp.dto.in.PedidoSemanalDtoIn;
-import com.alejobeliz.pentabyte.projects.mealapp.model.pedido.Pedido;
-import com.alejobeliz.pentabyte.projects.mealapp.repository.PedidoRepository;
+import com.alejobeliz.pentabyte.projects.mealapp.dto.out.PedidoSemanalDto;
 import com.alejobeliz.pentabyte.projects.mealapp.service.PedidoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -24,12 +20,32 @@ public class PedidoController {
         this.pedidoService = pedidoService;
     }
 
+    /*METODOS GET*/
+
+    @GetMapping("cliente/{idCliente}/ultimo")
+    public ResponseEntity<PedidoSemanalDto> getPedidoProximaSemana(@PathVariable Long idCliente) {
+        PedidoSemanalDto pedido = pedidoService.getPedidoProximaSemana(idCliente);
+        return ResponseEntity.ok(pedido);
+    }
+
+
+    /*METODOS POST*/
+
     @PostMapping
     @Transactional
-    public ResponseEntity crearPedidoSemanal(@RequestBody @Valid PedidoSemanalDtoIn pedido){
+    public ResponseEntity crearPedidoSemanal(@RequestBody @Valid PedidoSemanalDtoIn pedido) {
         pedidoService.crearNuevoPedido(pedido);
         return ResponseEntity.ok(pedido);
     }
+
+    /*METODOS DELETE*/
+    @DeleteMapping("cliente/{idCliente}/ultimo")
+    @Transactional
+    public ResponseEntity eliminarPedido(@PathVariable Long idCliente) {
+        pedidoService.eliminarUlitmoPedido(idCliente);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
