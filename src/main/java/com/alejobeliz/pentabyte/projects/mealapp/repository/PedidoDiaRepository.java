37d2f45efcfe642b0eb9f825a1f5fe70b5b3d18b
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-10-01 15:52:25 -0300

    Agregar quey findDetallePEdidosByPedidoDiaId

--- src/main/java/com/alejobeliz/pentabyte/projects/mealapp/repository/PedidoDiaRepository.java
+++ src/main/java/com/alejobeliz/pentabyte/projects/mealapp/repository/PedidoDiaRepository.java
@@ -2,6 +2,16 @@ package com.alejobeliz.pentabyte.projects.mealapp.repository;
 
 import com.alejobeliz.pentabyte.projects.mealapp.model.pedidosdia.PedidoDia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PedidoDiaRepository extends JpaRepository<PedidoDia,Long> {
+    @Query("""
+        SELECT pd
+        FROM PedidoDia pd
+        WHERE pd.pedido.id = :id
+        """)
+    List<PedidoDia> findPedidoDiasByPedidoId(@Param("id") Long id);
 }
