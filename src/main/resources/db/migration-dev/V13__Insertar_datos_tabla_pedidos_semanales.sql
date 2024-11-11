de2fe1455272d0fda8d06298a072d3755feb56f2
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2024-11-11 02:47:52 -0300

    Cambios en las fechas de los inserts

--- src/main/resources/db/migration-dev/V13__Insertar_datos_tabla_pedidos_semanales.sql
+++ src/main/resources/db/migration-dev/V13__Insertar_datos_tabla_pedidos_semanales.sql
@@ -1,4 +1,4 @@
 -- Insertar pedidos
 INSERT INTO pedidos_semanales (fecha_pedido, semana_entrega, cliente_id) VALUES
-('2024-09-01','2024-09-10', 1),  -- Pedido del cliente con ID 1
-('2024-09-08','2024-09-11', 2);  -- Pedido del cliente con ID 2
+('2024-11-11','2024-11-18', 1),  -- Pedido del cliente con ID 1
+('2024-11-11','2024-11-18', 2);  -- Pedido del cliente con ID 2
--- src/main/resources/db/migration-dev/V16__Insertar_datos_tabla_favoritos.sql
+++ src/main/resources/db/migration-dev/V16__Insertar_datos_tabla_favoritos.sql
@@ -1,6 +1,22 @@
--- Insertar favoritos
 INSERT INTO `favoritos` (`cliente_id`, `plato_id`) VALUES
 (1, 1), -- Favorito: Milanesa para el cliente con ID 1
 (1, 3), -- Favorito: Coca Cola para el cliente con ID 1
+(1, 4), -- Favorito: Pizza para el cliente con ID 1
+(1, 6), -- Favorito: Empanadas para el cliente con ID 1
+(1, 8), -- Favorito: Helado para el cliente con ID 1
+(1, 10), -- Favorito: Pastel para el cliente con ID 1
+(1, 12), -- Favorito: Café para el cliente con ID 1
+(1, 7), -- Favorito: Sopa para el cliente con ID 1
+(1, 9), -- Favorito: Hamburguesa para el cliente con ID 1
+(1, 11), -- Favorito: Té para el cliente con ID 1
+
 (2, 2), -- Favorito: Papas Fritas para el cliente con ID 2
-(2, 5); -- Favorito: Ensalada para el cliente con ID 2
+(2, 5), -- Favorito: Ensalada para el cliente con ID 2
+(2, 1), -- Favorito: Milanesa para el cliente con ID 2
+(2, 4), -- Favorito: Pizza para el cliente con ID 2
+(2, 6), -- Favorito: Empanadas para el cliente con ID 2
+(2, 8), -- Favorito: Helado para el cliente con ID 2
+(2, 10), -- Favorito: Pastel para el cliente con ID 2
+(2, 3), -- Favorito: Coca Cola para el cliente con ID 2
+(2, 9), -- Favorito: Hamburguesa para el cliente con ID 2
+(2, 12); -- Favorito: Café para el cliente con ID 2
